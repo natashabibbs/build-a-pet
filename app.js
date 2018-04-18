e36a88c3d8e0d3f632e94604e50e21b5c9cda4cc
Natasha Bibbs <natashabibbs10@gmail.com>
2018-04-17 14:01:28 -1000

    updated html

---
+++ app.js
@@ -0,0 +1,10 @@
+ window.onload = function(){
+   var name = document.getElementById("name");
+ 
+   var form = document.getElementById("myForm");
+   console.log(name.value);
+
+   var button = document.getElementById("button");
+
+   button.addEventListener("submit");
+}
