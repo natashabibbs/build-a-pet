3f7ef49c33a69857a719039e33f2118338c47fcf
Natasha Bibbs <natashabibbs10@gmail.com>
2018-04-24 13:50:42 -1000

    data printed in the console

--- app.js
+++ app.js
@@ -1,10 +1,46 @@
  window.onload = function(){
    var name = document.getElementById("name");
- 
    var form = document.getElementById("myForm");
-   console.log(name.value);
+   var species = document.getElementById("species");
+   var numOfLegs = document.getElementsByClassName("numberOfLegs");
+   var specialAbilities = document.getElementsByClassName("specialAbilities");
+   var email = document.getElementById("petEmail");
+   var password = document.getElementById("petPassword");
+   var description = document.getElementById("description");
+   var result = document.getElementById("result");
+   var obj = {};
 
    var button = document.getElementById("button");
 
-   button.addEventListener("submit");
-}
+   form.addEventListener("submit", function(event) {
+      event.preventDefault();
+      obj.name = name.value;
+      obj.species = species.value;
+      obj.email = email.value;
+      obj.password = password.value;
+      obj.description = description.value;
+      console.log(obj);
+
+      for (let i = 0; i < numOfLegs.length; i++){
+        if (numOfLegs[i].checked === true){
+          obj.numOfLegs = numOfLegs[i].value;
+        }
+      }
+
+      for (let i = 0; i < specialAbilities.length; i++){
+        if (specialAbilities[i].checked === true){
+          obj.specialAbilities = specialAbilities[i].value;
+        }
+      }
+      
+      
+
+   });
+};
+
+// var obj = {};
+
+// console.log('obj', obj);
+// obj.petName = "ShockWave";
+
+// console.log
